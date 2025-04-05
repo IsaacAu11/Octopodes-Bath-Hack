0ee9647d4a5bcdf270eabac5202c8a8bda18b05f
Jeebo17 <jeeboplays@gmail.com>
2025-04-05 13:36:06 +0100

    Commit initial UI

---
+++ octopodes-bath-hack/src/pages/frontPage.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import './frontPage.css';
+
+function FrontPage() {
+    const [isOnFrontPage, setIsOnFrontPage] = useState(true);
+    
+    return (
+        <>  
+            <h1 className={`title ${isOnFrontPage ? '' : 'on-front-page'}`} >Start your story:</h1>
+
+            <input 
+                type="text" 
+                className={`input ${isOnFrontPage ? '' : 'on-front-page'}`} 
+                placeholder="Enter your story here..." 
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        setIsOnFrontPage(false);
+                        e.preventDefault();
+                    }
+                }}
+            />
+        </>
+    );
+}
+
+export default FrontPage;
