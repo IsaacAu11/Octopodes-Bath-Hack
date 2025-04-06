143e8e984d7869f1ced6b49a02b6ca9635ee91d7
ThornRoutila <thornroutila@gmail.com>
2025-04-06 08:13:05 +0100

    test

--- octopodes-bath-hack/backend/node.py
+++ octopodes-bath-hack/backend/node.py
@@ -25,6 +25,8 @@ class Node():
         pass
     def json(self):
         pass
+    def __str__(self):
+        pass
 
 class FullNode(Node):
     def __init__(self, x, y, location = None):
@@ -38,7 +40,10 @@ class FullNode(Node):
         self.location = location
 
     def json(self):
+        print(self.x, self.y)
         return self.location
+    def __str__(self):
+        return str(self.location)
 
 class EmptyNode(Node):
     def __init__(self, x, y, name=None, eventType=None, description=None):
@@ -47,10 +52,13 @@ class EmptyNode(Node):
     def is_empty(self):
         return True
     def json(self):
+        print(self.x, self.y)
         location = {"location" : "",
                     "description" : "",
                     "characters" : []}
         return location
+    def __str__(self):
+        return "Empty"
 
 class Map():
     def __init__(self):
@@ -58,33 +66,34 @@ class Map():
         for i in range(9):
             self.map.append([None] * 9)
         self.cols_added_left = 0
-        self.cols_added_right = 0
+        
         self.rows_added_above = 0
-        self.rows_added_below = 0
+        
         self.map[4][4] = FullNode(4, 4)
     def getcell(self, x,y):
-        return self.map[x + self.cols_added_left][y + self.rows_added_below]
+        print(x + self.cols_added_left, y + self.rows_added_above)
+        return self.map[x + self.cols_added_left][y + self.rows_added_above]
     def cartesian_to_index(self, x, y):
-        return x + self.cols_added_left, y + self.rows_added_below
+        return x + self.cols_added_left, y + self.rows_added_above
  
     def calculate_neighbours(self,currentNode):
         neighbours = [None] * 8
         x, y = currentNode.get_location()
         if y > 0 - self.rows_added_above:
             neighbours[0] = self.getcell(y-1,x)
-            if x < len(self.map[y])-self.cols_added_right-1:
+            if x < len(self.map[y])-self.cols_added_left-1:
                 neighbours[1] = self.getcell(y-1,x+1)
             if x > 0 - self.cols_added_left:
                 neighbours[7] = self.getcell(y-1,x-1)
-        if y < len(self.map)-self.cols_added_left - 1:
+        if y < len(self.map)-self.rows_added_above - 1:
             neighbours[4] = self.getcell(y+1,x)
-            if x < len(self.map[y])-self.cols_added_right-1:
+            if x < len(self.map[y])-self.cols_added_left-1:
                 neighbours[3] = self.getcell(y+1,x+1)
             if x > 0 - self.cols_added_left:
                 neighbours[5] = self.getcell(y+1,x-1)
         if x > 0 - self.cols_added_left:
             neighbours[6] = self.getcell(y,x-1)
-        if x < len(self.map[0])-self.cols_added_right-1:
+        if x < len(self.map[0])-self.cols_added_left-1:
             neighbours[2] = self.getcell(y,x+1)
         return neighbours
     def __str__(self):
@@ -92,23 +101,30 @@ class Map():
         for line in self.map:
             s += str(line) + "\n"
         return s
+    def get_dimension(self):
+        return len(self.map[0]), len(self.map)
 
-    
+    # something wrong with how coordinates calculated
     # places a node next to the x,y node dependent on the location parameter as defined at the start
     def placenode(self, x, y, location, node):
         xadj, yadj = get_adjustment(location)
-        ix, iy = self.cartesian_to_index(x, y)
-        if ix + xadj < 0 - self.cols_added_left:
+        ix, iy = self.cartesian_to_index(x + xadj, y + yadj)
+        """if ix + xadj < 0:
             self.add_col_left()
-            ix, iy = self.cartesian_to_index(x, y)
-        if iy + yadj < 0 - self.rows_added_above:
-            self.add_row_above()
-            ix, iy = self.cartesian_to_index(x, y)
-        if ix + xadj  == len(self.map[0])-self.cols_added_right-1:
+            
+        
+        if iy + yadj < 0:
+            self.add_row_above() 
+           
+            
+        if ix + xadj  == len(self.map[0]) - 1:
             self.add_col_right()
-        if iy + yadj  == len(self.map)-self.cols_added_left - 1:
+        if iy + yadj  == len(self.map) - 1:
             self.add_row_below()
-        self.map[xadj + ix][yadj + iy] = node
+        ix, iy = self.cartesian_to_index(x, y)
+        print(xadj + ix , yadj + iy)
+        self.map[xadj + ix][yadj + iy] = node"""
+        self.map[ix][iy] = node
 
     def get_map(self):
         return self.map
@@ -116,8 +132,8 @@ class Map():
         self.map.insert(0, [None] * len(self.map[0]))
         self.rows_added_above += 1
     def add_row_below(self):
-        self.map.append(0, [None] * len(self.map[0]))
-        self.rows_added_below += 1
+        self.map.append([None] * len(self.map[0]))
+        
     def add_col_left(self):
         for row in self.map:
             row.insert(0, None)
@@ -125,6 +141,7 @@ class Map():
     def add_col_right(self):
         for row in self.map:
             row.append(None)
+      
             
 def get_adjustment(location):
     if location == 0:
@@ -168,10 +185,21 @@ class NodeGenerator():
         #locations_to_place = random.sample(placable_nodes, 4)
         print(locations_to_place)
         for location in placable_nodes:
-            print(location)
+            """print(location)
             newX, newY = get_adjustment(location)
             newX += x
-            newY += y
+            newY += y"""
+            newX, newY = map.cartesian_to_index(x,y)
+            if newX == 0:
+                map.add_col_left()
+            if newY == 0:
+                map.add_row_above()
+            w, h = map.get_dimension()
+            if newX == w:
+                map.add_col_right()
+            if newY == h:
+                map.add_col_below()
+            print(newX, newY)
             if location in locations_to_place:
                 map.placenode(x, y, location, FullNode(newX, newY,))
             else:
@@ -181,13 +209,14 @@ class NodeGenerator():
 
 class Game():
     def __init__(self):
-        self.current_x = 4
-        self.current_y = 4
+        self.current_x = 0
+        self.current_y = 0
         self.locations = []
         self.characters = []
-        self.map = Map()
+        self.map = None
         
     def start(self, locations, characters):
+        self.map = Map()
         self.current_x = 4
         self.current_y = 4
         self.locations = locations
@@ -199,7 +228,7 @@ class Game():
         self.locations.pop(0)
         NodeGenerator.generate_nodes(self.map, currentCell)
         #return self.return_state(0, 0)
-        return self.return_state(4, 4)
+        return self.return_state(self.current_x, self.current_y)
 
     def move(self, changeX, changeY):
         self.current_x += changeX
@@ -207,7 +236,7 @@ class Game():
         currentCell = self.map.getcell(self.current_x, self.current_y)
         NodeGenerator.generate_nodes(self.map, currentCell)
         if currentCell.get_discovered():
-            return self.return_state(currentCell)
+            return self.return_state(self.current_x, self.current_y)
         else:
             num = random.randint(0, len(self.locations) - 1)
             currentCell.discover(self.locations[num])
@@ -218,6 +247,7 @@ class Game():
     def return_state(self, x, y):
         print(self.map)
         json_map = {"locations": {
+            
             "[0, 0]": self.map.getcell(x-1, y-1).json(),
             "[1, 0]": self.map.getcell(x, y-1).json(),
             "[2, 0]": self.map.getcell(x+1, y-1).json(),
